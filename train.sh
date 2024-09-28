3e832e4eb31d5ce1fbeaa4569638307df6712238
da02345 <digvijaysinh.ajarekar@stud.th-deg.de>
2024-09-28 19:06:49 +0200

    updated train.sh

--- train.sh
+++ train.sh
@@ -5,8 +5,9 @@ source ./kgreedy/bin/activate
 # Array of strategies and arguments
 dataset=("microgrid" "checkerboard" "linear" "powerline")
 strategies=("approx_greedy" "random")
-args=(1 2 4 6 8 10 12)
+args=(1 2 4 8 12)
+ansatz=("strong_entangled", "tutorial_ansatz", "basic_entangled")
 
 # Run the Python script in parallel with different strategies and arguments
-parallel python3 main.py {1} {2} {3} ::: "${dataset[@]}"  ::: "${strategies[@]}" ::: "${args[@]}"
+parallel --jobs 30 --bar python3 main.py {1} {2} {3} {4}::: "${dataset[@]}"  ::: "${strategies[@]}" ::: ${ansatx[@]} ::: "${args[@]}"
 
